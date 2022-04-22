1ad08f85f3c7ba96c99d78078d96e407484b2076
ozkurtomer <ozkurtomer@outlook.com>
2022-04-23 01:00:43 +0300

    Add project files.

---
+++ NetCoreBackendTemplate.Business/Aspects/Autofac/SecuredOperation.cs
@@ -0,0 +1,36 @@
+﻿using Castle.DynamicProxy;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCoreBackendTemplate.Business.Aspects.Autofac
+{
+    public class SecuredOperation : MethodInterception
+    {
+        private string[] _roles;
+        private IHttpContextAccessor _httpContextAccessor;
+
+        public SecuredOperation(string roles)
+        {
+            _roles = roles.Split(',');
+            _httpContextAccessor = ServiceTool.ServiceProvider.GetService<IHttpContextAccessor>();
+
+        }
+
+        protected override void OnBefore(IInvocation invocation)
+        {
+            var roleClaims = _httpContextAccessor.HttpContext.User.ClaimRoles();
+            foreach (var role in _roles)
+            {
+                if (roleClaims.Contains(role))
+                {
+                    return;
+                }
+            }
+            throw new Exception("Yetkiniz yok");
+        }
+    }
+}
---
+++ NetCoreBackendTemplate.Entities/Classes/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCoreBackendTemplate.Entities.Classes
+{
+    public class Product : IEntity
+    {
+        public int Id { get; set; }
+        public string Product_Name { get; set; }
+        public int Product_CategoryId { get; set; }
+        public decimal Product_Price { get; set; }
+    }
+}
